59e5b63766b816f77e6971eb5fa302531647dfe0
Joshua Beighton <joshualbeighton@gmail.com>
2025-01-05 13:24:33 +0000

    Quiz functionality

--- index.ts
+++ index.ts
@@ -1,105 +1,181 @@
 import express, { Request, Response } from "npm:express";
 
 const app = express();
+
+// Middleware for JSON parsing.
 app.use(express.json());
+// Middleware to serve static files.
 app.use(express.static("public"));
 
+// Store the players currently in the game.
 const currentPlayers: string[] = [];
 
-let gameStarted = false;
-let questionNo = 0;
-let currentQuestion;
+// Store the responses of players to each question.
+const allResponses: Map<string, Map<string, string>> = new Map<
+    string,
+    Map<string, string>
+>();
+const currentResponses: Map<string, string> = new Map<string, string>();
 
+// Store whether the game has started.
+let gameStarted = false;
+// Store the current question number.
+let questionNo = 0;
+// Store the current question.
+let currentQuestion: string;
+
+/**
+ * Deal with host requesting new client.
+ */
 app.post("/hostgame/clientwaiting", async (req: Request, res: Response) => {
-    console.log("Waiting for player.");
     try {
+        // Wait for a new player to join the game, storing their name once they do.
         const nextPlayer = await waitForNewPlayers(req.body.players);
-        console.log("Returning");
+        // Send the player's name to the host.
         res.send(nextPlayer);
-    } catch (_error) {
+    } // If game is started before this request is fulfilled, send a 201 in response.
+    catch (_error) {
         res.status(201);
         res.send("Game Started");
     }
 });
 
+/**
+ * Handle a host request to start the game.
+ */
 app.post("/hostgame/startgame", (_req: Request, res: Response) => {
+    // Send an acknowledgement.
     res.send("ack");
+    // Store that the game has started.
     gameStarted = true;
 });
 
+/**
+ * Wait for new players to join.
+ * @param players The amount of players at the beginning of waiting.
+ * @returns The name of the newest player.
+ */
 function waitForNewPlayers(players: number) {
     return new Promise((resolve, reject) => {
         const interval = setInterval(() => {
+            // If the game has started, clear the interval and reject the promise.
             if (gameStarted) {
                 clearInterval(interval);
                 reject("GAME STARTED");
             }
 
+            // If a new player has joined, clear the interval and resolve the promise, returning the last element in the player list.
             if (players != currentPlayers.length) {
                 clearInterval(interval);
                 resolve(currentPlayers[currentPlayers.length - 1]);
             }
-        }, 50); // Check every 50ms
+        }, 100); // Check every 100ms
     });
 }
 
+/**
+ * Wait for the game to start.
+ * @returns "GAME STARTED"
+ */
 function waitForGameStarting() {
     return new Promise((resolve, _reject) => {
         const interval = setInterval(() => {
-            console.log(gameStarted);
+            // If the game has started, clear the interval and resolve the promise.
             if (gameStarted) {
                 clearInterval(interval);
                 resolve("GAME STARTED");
             }
-        }, 50); // Check every 50ms
+        }, 100); // Check every 100ms
     });
 }
 
-app.get("/playgame", async (_req: Request, res: Response) => {
-    const file = await (Deno.open("public/playGame/index.html"));
-    const decoder = new TextDecoder();
-    let toSend = "";
-    for await (const chunk of file.readable) {
-        toSend += decoder.decode(chunk);
-    }
-    res.send(toSend);
-});
+function waitForNextQuestion(oldQuestion: string) {
+    return new Promise((resolve, _reject) => {
+        const interval = setInterval(() => {
+            // If a new player has joined, clear the interval and resolve the promise, returning the last element in the player list.
+            if (oldQuestion != currentQuestion) {
+                clearInterval(interval);
+                resolve(currentQuestion);
+            }
+        }, 100); // Check every 100ms
+    });
+}
 
-app.post("/playgame/waitForStart", async (_req: Request, res: Response) => {
-    console.log("Client is waiting for game to start!");
+function waitForAnswers() {
+    return new Promise((resolve, _reject) => {
+        const interval = setInterval(() => {
+            // If a new player has joined, clear the interval and resolve the promise, returning the last element in the player list.
+            if (currentResponses.size == currentPlayers.length) {
+                clearInterval(interval);
+                allResponses.set(currentQuestion, currentResponses);
+                resolve(currentQuestion);
+            }
+        }, 100); // Check every 100ms
+    });
+}
+
+/**
+ * Handle get request to determine start of game.
+ */
+app.get("/playgame/waitForStart", async (_req: Request, res: Response) => {
     try {
+        // Wait for game to start.
         await waitForGameStarting();
-        console.log("Starting game for client");
+        // Respond to request.
         res.send("GAME STARTED");
-    } catch (_error) {
+    } // Response with error if necessary.
+    catch (_error) {
         res.status(501);
         res.send("Error");
     }
 });
 
+/**
+ * Handle new clients joining.
+ */
 app.post("/playgame/newclient", function (req: Request, res: Response) {
+    // Add the players name to the array, and respond with the list of current players.
     currentPlayers.push(req.body.Name);
     res.send(currentPlayers);
 });
 
+/**
+ * Handle a new incoming question.
+ */
 app.post("/hostgame/question", function (req: Request, res: Response) {
+    // Store the current question.
     currentQuestion = req.body;
+    // Empty the map of answers.
+    currentResponses.clear();
+    // Increment the question number.
     questionNo++;
-    console.log(currentQuestion);
+    // Respond to request.
     res.send("Ack");
 });
 
+/**
+ * Respond to the host waiting for answers.
+ */
 app.post(
     "/hostgame/awaitAnswers",
     async function (_req: Request, res: Response) {
         console.log("Waiting for responses");
-        await delay(2000);
+        await waitForAnswers();
         res.send("Boo");
     },
 );
 
-function delay(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
+app.get("/playgame/nextQuestion", async (_req: Request, res: Response) => {
+    await waitForNextQuestion(currentQuestion);
+    res.send(currentQuestion);
+});
 
+app.post("/playgame/answer", (req: Request, res: Response) => {
+    const player = req.body.Player;
+    const answer = req.body.Answer;
+    currentResponses.set(player, answer);
+    res.send("Ack");
+});
+
+// Listen on port 8000.
 app.listen(8000);
--- public/playGame/index.js
+++ public/playGame/index.js
@@ -1,3 +1,4 @@
+let globalUsername = "";
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.querySelector("#join_button");
     button.addEventListener("click", () => {
@@ -8,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function alertServer() {
     const username = document.querySelector("#input_name").value;
+    globalUsername = username;
     if (username.length > 0) {
         const req = new XMLHttpRequest();
         req.open("POST", "/playgame/newclient", true);
@@ -41,12 +43,96 @@ function waitForGame() {
             reject("Network error occurred.");
         };
 
-        req.open("POST", "/playgame/waitForStart");
+        req.open("GET", "/playgame/waitForStart");
         req.setRequestHeader("Content-Type", "application/json");
         req.send();
     });
 }
 
+function clearScreen() {
+    const body = document.querySelector("body");
+    while (body.children.length > 1) {
+        body.removeChild(body.children[1]);
+    }
+}
+
+function waitForNextQuestion() {
+    clearScreen();
+    return new Promise((resolve, reject) => {
+        const req = new XMLHttpRequest();
+        req.timeout = 600000;
+        req.onload = () => {
+            if (req.status === 200) {
+                resolve(req.response); // Resolve with the new player's data
+            } else if (req.status === 201) {
+                resolve();
+            } else {
+                reject(`Error: ${req.statusText} (${req.status})`);
+            }
+        };
+
+        req.onerror = () => {
+            reject("Network error occurred.");
+        };
+
+        req.open("GET", "/playgame/nextQuestion");
+        req.setRequestHeader("Content-Type", "application/json");
+        req.send();
+    });
+}
+
+function showQuestion(question) {
+    const questionText = document.createElement("h3");
+    questionText.id = "question";
+    questionText.innerText = question.question.Question;
+    const answersDiv = document.createElement("div");
+    for (index in question.question.Answers) {
+        element = document.createElement("button");
+        element.innerText = question.question.Answers[index];
+        element.addEventListener("click", answerButtonHandler);
+        answersDiv.appendChild(element);
+    }
+    document.querySelector("body").appendChild(questionText);
+    document.querySelector("body").appendChild(answersDiv);
+}
+
+function sendAnswer(answer) {
+    return new Promise((resolve, reject) => {
+        const req = new XMLHttpRequest();
+        req.timeout = 600000;
+        req.onload = () => {
+            if (req.status === 200) {
+                resolve(req.response); // Resolve with the new player's data
+            } else if (req.status === 201) {
+                resolve();
+            } else {
+                reject(`Error: ${req.statusText} (${req.status})`);
+            }
+        };
+
+        req.onerror = () => {
+            reject("Network error occurred.");
+        };
+
+        req.open("POST", "/playgame/answer");
+        req.setRequestHeader("Content-Type", "application/json");
+        req.send(JSON.stringify(answer));
+    });
+}
+
+function answerButtonHandler() {
+    const question = document.querySelector("#question").innerText;
+    const answer = this.innerText;
+    const ans = {
+        "Question": question,
+        "Answer": answer,
+        "Player": globalUsername,
+    };
+    sendAnswer(ans).then(startGame);
+}
+
 function startGame() {
-    alert("Game Started!");
+    waitForNextQuestion().then((response) => {
+        showQuestion(JSON.parse(response));
+    });
 }
